9681c7ded55e409b249f4ae287281bd788a9ca85
DAVID-TECH-GURU <davidmaina823123@gmail.com>
2023-04-22 17:28:45 +0300

    Done with assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    console.log("useEffect called");
+    console.log('Component rendered');
   });
 
-  console.log("Component rendering");
+  return <div>Hello, World!</div>;
+}
 
-  return <button>Click Me</button>;
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Count is now ${count}`);
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    console.log('Component mounted');
+
+    return () => {
+      console.log('Component unmounted');
+    };
+  }, []);
+
+  return <div>Hello, World!</div>;
+}
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
